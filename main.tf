9699b2092e478a5d7cd1cffdcad936f19fcef923
Loren Gordon <lorengordon@users.noreply.github.com>
2018-03-22 20:48:19 -0400

    Uses new-style index syntax rather than element function

--- main.tf
+++ main.tf
@@ -17,7 +17,7 @@ resource "aws_s3_bucket" "this" {
 }
 
 module "file_cache" {
-  source = "git::https://github.com/plus3it/terraform-external-file-cache?ref=1.0.1"
+  source = "git::https://github.com/plus3it/terraform-external-file-cache?ref=1.0.2"
 
   uris = "${local.uris}"
 }
@@ -34,9 +34,9 @@ data "null_data_source" "files" {
   count = "${length(local.uris)}"
 
   inputs {
-    filepath = "${element(module.file_cache.filepaths, count.index)}"
-    filename = "${replace(basename(element(module.file_cache.filepaths, count.index)), "%20", " ")}"
-    hash_content = "${sha512(element(module.file_cache.filepaths, count.index))} ${replace(basename(element(module.file_cache.filepaths, count.index)), "%20", " ")}"
+    filepath = "${module.file_cache.filepaths[count.index]}"
+    filename = "${replace(basename(module.file_cache.filepaths[count.index]), "%20", " ")}"
+    hash_content = "${sha512(module.file_cache.filepaths[count.index])} ${replace(basename(module.file_cache.filepaths[count.index]), "%20", " ")}"
   }
 }
 
@@ -44,17 +44,17 @@ resource "aws_s3_bucket_object" "files" {
   count = "${length(data.null_data_source.files.*.outputs.filepath)}"
 
   bucket = "${aws_s3_bucket.this.id}"
-  key    = "${var.prefix}${element(local.s3_paths, count.index)}${element(data.null_data_source.files.*.outputs.filename, count.index)}"
-  source = "${element(data.null_data_source.files.*.outputs.filepath, count.index)}"
-  etag   = "${md5(file(element(data.null_data_source.files.*.outputs.filepath, count.index)))}"
+  key    = "${var.prefix}${local.s3_paths[count.index]}${data.null_data_source.files.*.outputs.filename[count.index]}"
+  source = "${data.null_data_source.files.*.outputs.filepath[count.index]}"
+  etag   = "${md5(file(data.null_data_source.files.*.outputs.filepath[count.index]))}"
 }
 
 resource "aws_s3_bucket_object" "hashes" {
   count = "${length(data.null_data_source.files.*.outputs.filepath)}"
 
   bucket       = "${aws_s3_bucket.this.id}"
-  key          = "${var.prefix}${element(local.s3_paths, count.index)}${element(data.null_data_source.files.*.outputs.filename, count.index)}.SHA512"
-  content      = "${element(data.null_data_source.files.*.outputs.hash_content, count.index)}"
+  key          = "${var.prefix}${local.s3_paths[count.index]}${data.null_data_source.files.*.outputs.filename[count.index]}.SHA512"
+  content      = "${data.null_data_source.files.*.outputs.hash_content[count.index]}"
   content_type = "application/octet-stream"
-  etag         = "${md5("${element(data.null_data_source.files.*.outputs.hash_content, count.index)}")}"
+  etag         = "${md5(data.null_data_source.files.*.outputs.hash_content[count.index])}"
 }
